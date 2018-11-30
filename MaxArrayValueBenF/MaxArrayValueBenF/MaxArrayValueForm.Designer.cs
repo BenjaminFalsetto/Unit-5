d89a6ab885b61af524e623e3f4e86990917201fd
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-30 10:59:07 -0500

    Nothing is working anymore

---
+++ MaxArrayValueBenF/MaxArrayValueBenF/MaxArrayValueForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MaxArrayValueBenF
+{
+    partial class frmMaxArrayValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMaxVal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(134, 61);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(111, 124);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMaxVal
+            // 
+            this.lblMaxVal.AutoSize = true;
+            this.lblMaxVal.Location = new System.Drawing.Point(126, 252);
+            this.lblMaxVal.Name = "lblMaxVal";
+            this.lblMaxVal.Size = new System.Drawing.Size(90, 13);
+            this.lblMaxVal.TabIndex = 2;
+            this.lblMaxVal.Text = "The max value is:";
+            this.lblMaxVal.Visible = false;
+            // 
+            // frmMaxArrayValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 303);
+            this.Controls.Add(this.lblMaxVal);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMaxArrayValue";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Max Array Value Benjamin Falsetto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMaxVal;
+    }
+}
+
---
+++ MaxArrayValueBenF/MaxArrayValueBenF/MaxArrayValueForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxArrayValueBenF
+{
+    public partial class frmMaxArrayValue : Form
+    {
+        public frmMaxArrayValue()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables and constants
+            const int MAX_ARRAY_SIZE = 10, MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int index, randomNumber;
+            Random rndNumGen = new Random();
+
+            randomNumber = rndNumGen.Next(1, 500 + 1);
+            this.lstNumbers.Items.Add(randomNumber);
+            Console.WriteLine(randomNumber);
+            //clear the listbox
+            //this.lstNumbers.Items.Clear();
+
+            //assign the numbers to the array
+            for (index = 0; index > MAX_ARRAY_SIZE; index++)
+            {
+                //generate a random number between 1 and the max array size
+                randomNumber = rndNumGen.Next(1, MAX_RANDOM_NUMBER + 1);
+                //Console.WriteLine("The random number is " + randomNumber);
+
+                //assign the random number to the cell at position index in the array
+                arrayOfIntegers[index] = randomNumber;
+
+                //add the random number to the listbox
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form to display the new item in the listbox
+                this.Refresh();
+
+                
+            }
+        }
+    }
+}
+

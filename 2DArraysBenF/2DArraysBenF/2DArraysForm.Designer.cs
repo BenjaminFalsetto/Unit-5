4b54cd8b23f0854e972e22d52a9f8878593d644a
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-12-04 10:42:00 -0500

    Update

---
+++ 2DArraysBenF/2DArraysBenF/2DArraysForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _2DArraysBenF
+{
+    partial class frm2DArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudHeight = new System.Windows.Forms.NumericUpDown();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudHeight
+            // 
+            this.nudHeight.Location = new System.Drawing.Point(32, 43);
+            this.nudHeight.Name = "nudHeight";
+            this.nudHeight.Size = new System.Drawing.Size(120, 20);
+            this.nudHeight.TabIndex = 0;
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(32, 135);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(32, 95);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(120, 20);
+            this.nudWidth.TabIndex = 2;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(73, 27);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 3;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(75, 79);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 4;
+            this.lblWidth.Text = "Width";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(55, 236);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // frm2DArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 280);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.nudHeight);
+            this.Name = "frm2DArrays";
+            this.Text = "2D Arrays Benjamin Falsetto";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudHeight;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ 2DArraysBenF/2DArraysBenF/2DArraysForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DArraysBenF
+{
+    public partial class frm2DArrays : Form
+    {
+        Random rndNumGen;
+        public frm2DArrays()
+        {
+            InitializeComponent();
+            rndNumGen = new Random();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int width;
+            int length;
+            
+        }
+    }
+}

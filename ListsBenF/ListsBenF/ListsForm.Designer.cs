4f57a4f0dfdf6c4068e1c2cf4e68cbe39ad5942c
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-12-05 10:51:41 -0500

    Update

---
+++ ListsBenF/ListsBenF/ListsForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ListsBenF
+{
+    partial class frmLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(90, 102);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(120, 95);
+            this.lstMarks.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(113, 203);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(100, 76);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 2;
+            // 
+            // frmLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 298);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lstMarks);
+            this.Name = "frmLists";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lists Benjamin Falsetto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtMark;
+    }
+}
+
---
+++ ListsBenF/ListsBenF/ListsForm.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by: Benjamin Falsetto
+ * Created on: 2018/12/05
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Lists
+ * This program accepts marks between 0 and 100 and then puts it
+ * into a list then into a listbox. If its not within the range it throws an error
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsBenF
+{
+    public partial class frmLists : Form
+    {
+        List<int> listOfMarks = new List<int>();
+
+        public frmLists()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int userMark = -1;
+
+            //convert toe vaue in the textboc to an integer
+            try
+            {
+                userMark = int.Parse(txtMark.Text);
+            }
+            //if the user didn't enter a valid nymber, write the error to the command line
+            catch (Exception parseError)
+            {
+                Console.WriteLine("An error occured: '{0}'", parseError);
+            }
+
+            //check if the user entered a number between 0 and 100
+            if (userMark >= 0 && userMark <= 100)
+            {
+                //add the ,ark to the listbox
+                this.lstMarks.Items.Add(userMark);
+
+                //add the mark to the list
+                listOfMarks.Add(userMark);
+            }
+            //else, display an error
+            else
+            {
+                MessageBox.Show("Please enter a number between 0 and 100", "Invalid Number");
+            }
+        }
+    }
+}

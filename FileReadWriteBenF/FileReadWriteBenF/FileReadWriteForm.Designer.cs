b08e317724c30bc9596a4f12e8d5d11c2fa53a8f
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-12-14 16:08:37 -0500

    Update

--- FileReadWriteBenF/FileReadWriteBenF/FileReadWriteForm.Designer.cs
+++ FileReadWriteBenF/FileReadWriteBenF/FileReadWriteForm.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblCompare = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
             this.btnCheck = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblCompare
+            // lblInstructions
             // 
-            this.lblCompare.AutoSize = true;
-            this.lblCompare.Location = new System.Drawing.Point(57, 47);
-            this.lblCompare.Name = "lblCompare";
-            this.lblCompare.Size = new System.Drawing.Size(214, 26);
-            this.lblCompare.TabIndex = 0;
-            this.lblCompare.Text = "Compare strings on each line of input .txt file\r\n(Case doesn\'t matter)";
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(62, 29);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(211, 26);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Compare strings on each line of input.txt file\r\n(Case doesn\'t matter)";
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(127, 139);
+            this.btnCheck.Location = new System.Drawing.Point(130, 104);
             this.btnCheck.Name = "btnCheck";
             this.btnCheck.Size = new System.Drawing.Size(75, 23);
             this.btnCheck.TabIndex = 1;
@@ -51,15 +52,26 @@
             this.btnCheck.UseVisualStyleBackColor = true;
             this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(75, 173);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(185, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Output has been generated to text file";
+            this.lblOutput.Visible = false;
+            // 
             // frmFileReadWrite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 291);
+            this.ClientSize = new System.Drawing.Size(335, 261);
+            this.Controls.Add(this.lblOutput);
             this.Controls.Add(this.btnCheck);
-            this.Controls.Add(this.lblCompare);
+            this.Controls.Add(this.lblInstructions);
             this.Name = "frmFileReadWrite";
-            this.Text = "File Read Write Benjamin Falsetto";
+            this.Text = "Read & Write to/from Text Files";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,8 +79,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblCompare;
+        private System.Windows.Forms.Label lblInstructions;
         private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
